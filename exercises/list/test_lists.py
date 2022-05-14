6cc4c05b1e19615353f155eafeabe01218b988f4
Dorian Turba <dorian.turba@gmail.com>
2022-05-14 12:13:31 +0200

    update tests for list

--- exercises/list/test_lists.py
+++ exercises/list/test_lists.py
@@ -1,17 +1,16 @@
 from random import randint
 
+import pytest
 from pytest import mark
 
 from custom_classes.custom_list import list_
 
 
-def get_list(result: list):
-    assert isinstance(result, list)
-    assert len(result) >= 1
-    assert result[-1] is ...
-    assert result.count(...) == 1
-    result.pop()
-    return list(result)
+def check_list(result: list):
+    assert isinstance(result, list), "Result is not a list"
+    assert len(result) >= 1, "Result is empty, should be at least one ellipsis"
+    assert result[-1] is ..., "Last element must be an ellipsis"
+    assert result.count(...) == 1, "Too many ellipses"
 
 
 def to_custom_list(vanilla_list: list):
@@ -24,13 +23,39 @@ def to_custom_list(vanilla_list: list):
     randint(-1_000, -1),
 ])
 def test_my_list(module, size):
-    result = get_list(module.my_list(size))
-    assert result == [None] * size
+    result = module.my_list(size)
+    expected = [None] * size + [...]
+
+    def my_list(_):
+        return result
+
+    check_list(result)
+    assert my_list(size) == expected
 
 
 @mark.parametrize("integers", [
     [],
     [randint(-1_000, 1_000) for i in range(randint(5, 100))],
+    [randint(2 ** 30, 2 ** 60), randint(2 ** 30, 2 ** 60)],
 ])
 def test_my_sum(module, integers):
-    assert module.my_sum(to_custom_list(integers)) == sum(integers)
+    my_sum = module.my_sum
+    list_of_integers = to_custom_list(integers)
+    expected = sum(integers)
+    assert my_sum(list_of_integers) == expected
+
+
+@mark.parametrize("original_list, value", [
+    ([], randint(-1_000, 1_000)),
+    ([randint(1, 10) for _ in range(randint(5, 100))], randint(-1_000, 1_000)),
+])
+def test_my_insert(module, original_list, value):
+    some_list = to_custom_list(original_list)
+    result = module.my_insert(some_list, value)
+    expected = original_list + [value, ...]
+
+    def my_insert(_, __):
+        return result
+
+    check_list(result)
+    assert my_insert(some_list, value) == expected
