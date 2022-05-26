4170a8d60900a166715ba7a110583cab2a02c4ea
Dorian Turba <dorian.turba@gmail.com>
2022-05-26 21:00:33 +0200

    add string tests

--- exercises/str/test_str.py
+++ exercises/str/test_str.py
@@ -11,8 +11,8 @@ def check_str(result: str):
     assert result.count('\0') == 1, "Too many '\\0'"
 
 
-def to_custom_str(vanilla_list: str):
-    return vanilla_list + '\0'
+def to_custom_str(vanilla_str: str):
+    return vanilla_str + '\0'
 
 
 @mark.parametrize("string, expected", [
@@ -20,22 +20,21 @@ def to_custom_str(vanilla_list: str):
     ("", 0),
     ("Lorem ipsum dolor sit amet", 26),
 ])
-def test_my_list(module, string, expected):
+def test_string_length(module, string, expected):
     string = to_custom_str(string)
     result = module.string_length(string)
 
     def string_length(_):
         return result
 
-    check_str(result)
     assert string_length(string) == expected
 
 
 @mark.parametrize("string", [
-    (sample(ascii_letters, randint(10, 50))),
-    ([choice(ascii_lowercase) for _ in range(randint(1000, 50000))]),
+    (''.join(sample(ascii_letters, randint(10, 50)))),
+    (''.join([choice(ascii_lowercase) for _ in range(randint(1000, 2000))])),
 ])
-def test_my_list_rand(module, string):
+def test_string_length_rand(module, string):
     expected = len(string)
     string = to_custom_str(string)
     result = module.string_length(string)
@@ -43,5 +42,4 @@ def test_my_list_rand(module, string):
     def string_length(_):
         return result
 
-    check_str(result)
     assert string_length(string) == expected
